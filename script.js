4da28bd934b4e3cd5ba4233a69402c68b4241847
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-08-08 14:26:55 -0400

    Cleaned up the code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 // CODE BELOW THIS ONLY TAKES THE INPUTS AND DISPLAYS IT IN THE TABLE //
 
 
@@ -52,8 +53,6 @@
 // }
 
 
-
-
 // // CODE BELOW THIS IS UPDATED (NOT COMPLETED) VERION WHERE THE LOCAL STORAGE IS BEING USED //
 
 
@@ -76,7 +75,7 @@ const store_object = () => {
         let serial_number = arr_storage.length;
 
         console.log(serial_number);
-        
+
         let new_object = {
             serial : serial_number,
             task : input
@@ -92,11 +91,14 @@ const store_object = () => {
     document.getElementById("task_input").value = "";
     iterate();
 
+    iterate();
+
 }
 
 
 const create_new_row = (row) => {
 
+<<<<<<< HEAD
     console.log(row.task)
     
 
@@ -117,12 +119,37 @@ const create_new_row = (row) => {
                             <button class="act_btn" id="${row.serial}_remove" >Remove</button>`
     new_row.appendChild(actions_cell);
 
+=======
+    let table_body = document.getElementById("table_body");
+
+    let new_row = document.createElement("tr");
+
+    let serial_cell = document.createElement("td");
+    serial_cell.innerHTML = `<td>${row.serial+1}</td>`
+    new_row.appendChild(serial_cell);
+
+    let task_cell = document.createElement("td");
+    task_cell.innerHTML = `<td>${row.task}</td>`
+    new_row.appendChild(task_cell);
+
+    let actions_cell = document.createElement("td");
+    actions_cell.innerHTML = `<button class="act_btn" id="${row.serial}_done">Done</button>
+                            <button class="act_btn" id="${row.serial}_remove">Remove</button>`
+    new_row.appendChild(actions_cell);
+
+>>>>>>> c2fb5bf (Cleaned up the code)
     table_body.appendChild(new_row);
 
 }
 
 const iterate = () => {
+<<<<<<< HEAD
     document.getElementById("table_body").innerHTML = "";
+=======
+
+    document.getElementById("table_body").innerHTML = "";
+
+>>>>>>> c2fb5bf (Cleaned up the code)
     arr_storage = get_array();
 
     arr_storage.map((todo) => {
@@ -139,83 +166,3 @@ if (arr_storage == null) {
 }   else    {
     iterate();
 }
-
-
-
-// const table = document.getElementById("table");
-
-// // let arr = JSON.parse(localStorage.getItem(arr));
-
-// const get_array = () => {
-    
-//     return JSON.parse(localStorage.getItem(arr));
-// }
-
-// if (localStorage.getItem("arr") == null) {
-//     let arr = []
-//     localStorage.setItem("arr", arr);
-// } else {
-//     arr_storage = get_array();
-// }
-
-// const create_row = (todo, i) => {
-
-//     let table_body = document.getElementById("table_body");
-
-//     const new_row = document.createElement("tr");
-    
-//     new_serial_cell = document.createElement("td");
-//     new_serial_cell.innerHTML = `<td>${todo.serial}<td>`;
-//     new_row.appendChild(new_serial_cell);
-
-//     new_task_cell = document.createElement("td");
-//     new_task_cell.innerHTML = `<td>${todo.task}</td>`;
-//     new_row.appendChild(new_task_cell);
-
-//     action_buttons_cell = document.createElement("td");
-//     action_buttons_cell.innerHTML = `<td class="table_btn">
-//                                 <button type="sumbit" id="${todo.serial}_done">Done</button>
-//                                 <button type="submit" id="${todo.serial}_remove">Remove Task</button>
-//                                 </td>`;
-//     new_row.appendChild(action_buttons_cell);
-
-//     table_body.appendChild(new_row);
-
-//     arr_storage = [];
-
-//     document.getElementById("task_input").value = "";
-
-// }
-
-// const iterate = () => {
-
-//     let arr_storage = get_array();
-
-//     arr_storage.map((todo, i) => {
-    
-//         create_row(todo, i);
-
-//     })
-// }
-
-// const store_locally = () => {
-
-//     iterate(todo);
-    
-//     let input = document.getElementById("task_input").value;
-    
-//     if (input === "") {
-//         alert("Please enter a task before submitting!") 
-//     } else {
-//         let new_object = {
-//             serial : todo.serial,
-//             task : input
-//         }
-
-//         arr.push(new_object);
-        
-//         serial_number ++;
-
-//         localStorage.setItem("arr", JSON.stringify(arr));
-//     }
-// }
